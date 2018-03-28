436c9531b6cae76b25ef6d712c2a1ebda373dbab
unknown <baitao-lhq@sinosig.com>
2018-03-28 13:15:34 +0800

    init commit

---
+++ src/main/java/com/jamie/study/java8/Hello.java
@@ -0,0 +1,11 @@
+package com.jamie.study.java8;
+
+/**
+ * @author baitao
+ * @date 2018/3/28
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+}
